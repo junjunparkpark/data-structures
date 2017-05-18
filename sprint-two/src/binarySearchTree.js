a8753408b4987c864a795df0364d75e722316216
Hack Reactor Student <student@hackreactor.com>
2017-05-18 11:43:02 -0700

    finished Hash table

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,36 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+  
+  
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+  if (value > this.value) {
+    if (this.right instanceof BinarySearchTree) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  } else if (value < this.value) {
+    if (this.left instanceof BinarySearchTree) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
+};
+
+
 
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,21 +5,54 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  var bucket = [].concat(this._storage.get(index) || []);
+
+  if (getIndexByKey(bucket, k) === -1) {
+    bucket.push([k, v]);
+  } else {
+    bucket[getIndexByKey(bucket, k)] = [k, v];
+  }
+
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  //console.log(bucket[getIndexByKey(bucket, k)][1]);
+  var targetIndex = getIndexByKey(bucket, k);
+  if (targetIndex === -1) {
+    return undefined;
+  } 
+  return bucket[targetIndex][1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each((value, i, storage) => {
+  // console.log(this._storage.get(3));
+  // console.log(this._storage.get(1));
+  this._storage.each(function(bucket, i, storage) {
     if (i === index) {
-      delete storage[i];
+      var temp = getIndexByKey(storage[i], k);
+      if (temp === -1) {
+        throw new Error('key not found in Hash Table');
+      } else {
+        delete storage[i][temp];
+      }
+      
     }
   });
+  console.log(this._storage.get(3));
+};
+
+var getIndexByKey = function(arr, k) {
+  var result = -1;
+  arr.forEach(function(obj, index) {
+    if (obj[0] === k ) {
+      result = index;
+    } 
+  });
+  return result;
 };
 
 
